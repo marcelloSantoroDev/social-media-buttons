7be1ae72fafa8f0a4cd4e2d704196aeb5ab429f7
Marcello <marcelloaltermundi@gmail.com>
2023-05-17 18:00:59 -0300

    fix: configs

--- smbs/next.config.js
+++ smbs/next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.join(__dirname, 'src');
+        config.resolve.alias['@mui'] = path.join(__dirname, 'node_modules', '@mui');
+        return config;
+    }
+};
